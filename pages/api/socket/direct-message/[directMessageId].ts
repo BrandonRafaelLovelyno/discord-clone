25eb3ca7dc52a9f382cd1be082c7d35057a1918b
BrandonRafaelLovelyno <brandonrafael80@gmail.com>
2023-12-24 15:52:31 +0700

    <feat:socket api for direct message crud>

---
+++ pages/api/socket/direct-message/[directMessageId].ts
@@ -0,0 +1,86 @@
+import options from "@/lib/auth/option";
+import prismadb from "@/lib/orm/prismadb";
+import { NextApiResponseServerIo } from "@/lib/types/socket";
+import { NextApiRequest } from "next";
+import { getServerSession } from "next-auth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponseServerIo
+) {
+  try {
+    if (req.method !== "PATCH" && req.method !== "DELETE") {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid method",
+        data: {},
+      });
+    }
+    const session = await getServerSession(req, res, options);
+    if (!session) {
+      return res
+        .status(402)
+        .json({ success: false, message: "Unauthorized", data: {} });
+    }
+    const { conversationId, directMessageId } = req.query;
+    const { content, fileUrl } = req.body;
+
+    if (!content || !conversationId) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing fields",
+        data: {},
+      });
+    }
+    const currentMember = await prismadb.member.findFirst({
+      where: {
+        profileId: session.user.profileId,
+      },
+    });
+    if (!currentMember) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid session",
+        data: {},
+      });
+    }
+    let finalDirectMessage;
+
+    if (req.method == "PATCH") {
+      finalDirectMessage = await prismadb.directMessage.update({
+        where: {
+          id: directMessageId as string,
+        },
+        data: {
+          fileUrl: fileUrl as string,
+          content: content as string,
+        },
+      });
+    } else {
+      finalDirectMessage = await prismadb.directMessage.update({
+        where: {
+          id: directMessageId as string,
+        },
+        data: {
+          deleted: true,
+          content: "The message has been deleted",
+        },
+      });
+    }
+
+    const updateKey = `conversation:${conversationId}:directMessage:update`;
+
+    res.socket.server.io.emit(updateKey, finalDirectMessage);
+    return res.status(200).json({
+      success: true,
+      message: "",
+      data: finalDirectMessage,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      data: {},
+      message: (err as Error).message,
+    });
+  }
+}
---
+++ pages/api/socket/direct-message/index.ts
@@ -0,0 +1,71 @@
+import options from "@/lib/auth/option";
+import prismadb from "@/lib/orm/prismadb";
+import { NextApiResponseServerIo } from "@/lib/types/socket";
+import { NextApiRequest } from "next";
+import { getServerSession } from "next-auth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponseServerIo
+) {
+  try {
+    if (req.method !== "POST") {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid method",
+        data: {},
+      });
+    }
+    const session = await getServerSession(req, res, options);
+    if (!session) {
+      return res
+        .status(402)
+        .json({ success: false, message: "Unauthorized", data: {} });
+    }
+    const { conversationId } = req.query;
+    const { content, fileUrl } = req.body;
+
+    if (!content || !conversationId) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing fields",
+        data: {},
+      });
+    }
+    const currentMember = await prismadb.member.findFirst({
+      where: {
+        profileId: session.user.profileId,
+      },
+    });
+    if (!currentMember) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid session",
+        data: {},
+      });
+    }
+    const newDirectMessage = await prismadb.directMessage.create({
+      data: {
+        conversationId: conversationId as string,
+        content: content as string,
+        fileUrl: fileUrl as string,
+        memberId: currentMember.id,
+      },
+    });
+
+    const socketKey = `conversation:${conversationId}:directMessage`;
+
+    res.socket.server.io.emit(socketKey, newDirectMessage);
+    return res.status(200).json({
+      success: true,
+      message: "",
+      data: newDirectMessage,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      data: {},
+      message: (err as Error).message,
+    });
+  }
+}
