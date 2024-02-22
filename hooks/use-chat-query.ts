a0481905883212bd759a47971907ae3efa6ff1d1
BrandonRafaelLovelyno <brandonrafael80@gmail.com>
2024-02-22 10:02:17 +0700

    fix : 50ms refetch chat

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -42,7 +42,7 @@ const useChatQuery = ({
     queryKey: [queryKey],
     queryFn: fetchMessages,
     getNextPageParam: (lastPage) => lastPage?.data.nextCursor,
-    refetchInterval: isConnected ? false : 3000,
+    refetchInterval: 50,
     initialPageParam: "-1",
   });
   return {
