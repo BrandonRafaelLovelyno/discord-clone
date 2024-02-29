6de1787c6fde28ae14b1c88f645aa20a5cc2ab9a
BrandonRafaelLovelyno <brandonrafael80@gmail.com>
2024-02-29 15:49:35 +0700

    fix : polling socket badge

--- components/socket-badge.tsx
+++ components/socket-badge.tsx
@@ -6,17 +6,10 @@ import { Badge } from "./ui/badge";
 
 const SocketBadge = () => {
   const socket = useSocket();
-  const className: string = useMemo(() => {
-    return socket.isConnected
-      ? "bg-emerald-600 text-white border-none"
-      : "bg-yellow-600 text-white border-none";
-  }, [socket.isConnected]);
+  const className = "text-white border-none bg-emerald-600";
 
-  const text: string = useMemo(() => {
-    return socket.isConnected
-      ? "Live Real-time updates"
-      : "Fallback : polling every 1s";
-  }, [socket.isConnected]);
+  const text: string = "Live Real-time updates";
+
   return (
     <Badge className={className} variant="outline">
       {text}
